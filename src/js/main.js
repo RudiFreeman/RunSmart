15074592917b0d2e7b4766adad41448409175d12
AlekseiStrugovshchikov <swopfreeman@gmail.com>
2019-11-17 23:52:39 +0300

    feat: add section catalog | add toggle tabs | add toggle slide

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,35 @@
 $(document).ready(function () {
-    $('.carousel__inner').slick({
-        speed: 300,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icon/left-solid.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icon/right-solid.svg"></button>',
-        responsive: [
-            {
-              breakpoint: 992,
-              settings: {
-                dots: true,
-                arrows: false,
-              }
-            },
-          ]
+  $('.carousel__inner').slick({
+    speed: 300,
+    // adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icon/left-solid.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icon/right-solid.svg"></button>',
+    responsive: [{
+      breakpoint: 992,
+      settings: {
+        dots: true,
+        arrows: false,
+      }
+    }, ]
+  });
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(catalogItem) {
+    $(catalogItem).each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
     });
+  }
+
+  toggleSlide('.catalog-item__back');
+  toggleSlide('.catalog-item__link');
+
+
 });
-
-
